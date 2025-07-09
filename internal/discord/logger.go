fad031dd9a7d57500cb8dc2f8ebe395e3d054c02
Briggzee <briggzee@arcadia.belsco>
2025-07-09 15:21:59 +0800

    Subscribers more modular. Improve logging.

--- internal/discord/logger.go
+++ internal/discord/logger.go
@@ -1,7 +1,7 @@
-package commands
+package discord
 
 import (
 	"github.com/Zeethulhu/plebnet-discord-bot/internal/utils"
 )
 
-var logger = utils.NewLogger("Commands")
+var logger = utils.NewLogger("Discord")
